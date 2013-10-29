4bc2f8c5febdb11950d6966e353bf57895fdd942
Yongce Tu <yongce.tu@gmail.com>
2013-10-29 17:30:20 +0800

    change: Add IntentUtils

---
+++ src/me/ycdev/androidlib/utils/IntentUtils.java
@@ -0,0 +1,15 @@
+package me.ycdev.androidlib.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+public class IntentUtils {
+    public static boolean canStartActivity(Context cxt, Intent activityIntent) {
+        // Use PackageManager.MATCH_DEFAULT_ONLY to behavior same as Context#startAcitivty()
+        ResolveInfo resolveInfo = cxt.getPackageManager().resolveActivity(activityIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo != null;
+    }
+}
