1b5ee8aa9ee66283dc28c60845fbfaed951bdee4
Yongce Tu <yongce.tu@gmail.com>
2014-04-25 18:42:27 +0800

    change: Add more methods for IoUtils and new HttpClient

--- src/me/ycdev/androidlib/utils/IoUtils.java
+++ src/me/ycdev/androidlib/utils/IoUtils.java
@@ -3,13 +3,21 @@ package me.ycdev.androidlib.utils;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class IoUtils {
+    private static final int IO_BUF_SIZE = 1024 * 16; // 16KB
+
+    private IoUtils() {
+    }
+
     /**
      * Close the closeable target and eat possible exceptions.
      * @param target The target to close. Can be null.
@@ -70,6 +78,17 @@ public class IoUtils {
         }
     }
 
+    public static void createParentDirsIfNeeded(File file) {
+        File dirFile = file.getParentFile();
+        if (dirFile != null && !dirFile.exists()) {
+            dirFile.mkdirs();
+        }
+    }
+
+    public static void createParentDirsIfNeeded(String filePath) {
+        createParentDirsIfNeeded(new File(filePath));
+    }
+
     public static void saveAsFile(String content, String filePath) throws IOException {
         FileWriter fw = new FileWriter(filePath);
         try {
@@ -77,6 +96,41 @@ public class IoUtils {
             fw.flush();
         } finally {
             closeQuietly(fw);
+        }
+    }
+
+    /**
+     * Save the input stream into a file.</br>
+     * Note: This method will not close the input stream.
+     */
+    public static void saveAsFile(InputStream is, String filePath) throws IOException {
+        FileOutputStream fos = new FileOutputStream(filePath);
+        try {
+            copyStream(is, fos);
+        } finally {
+            closeQuietly(fos);
+        }
+    }
+
+    /**
+     * Copy data from the input stream to the output stream.</br>
+     * Note: This method will not close the input stream and output stream.
+     */
+    public static void copyStream(InputStream is, OutputStream os) throws IOException {
+        byte[] buffer = new byte[IO_BUF_SIZE];
+        int len = 0;
+        while ((len = is.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+        os.flush();
+    }
+
+    public static void copyFile(String srcFilePath, String destFilePath) throws IOException {
+        FileInputStream fis = new FileInputStream(srcFilePath);
+        try {
+            saveAsFile(fis, destFilePath);
+        } finally {
+            closeQuietly(fis);
         }
     }
 }
