35ca8ac33409fa3ce72fc187d573f9157c5fee93
Yongce Tu <yongce.tu@gmail.com>
2014-05-18 01:56:53 +0800

    change: Add WeakHandler

---
+++ src/me/ycdev/androidlib/base/WeakHandler.java
@@ -0,0 +1,26 @@
+package me.ycdev.androidlib.base;
+
+import java.lang.ref.WeakReference;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class WeakHandler extends Handler {
+    public interface MessageHandler {
+        void handleMessage(Message msg);
+    }
+
+    private WeakReference<MessageHandler> mTargetHandler;
+
+    public WeakHandler(MessageHandler msgHandler) {
+        mTargetHandler = new WeakReference<MessageHandler>(msgHandler);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        MessageHandler realHandler = mTargetHandler.get();
+        if (realHandler != null) {
+            realHandler.handleMessage(msg);
+        }
+    }
+}
