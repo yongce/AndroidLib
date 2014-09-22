7ee2effe1e45b5aa6ac576a34089fcca23d60369
Yongce Tu <yongce.tu@gmail.com>
2014-09-22 19:23:13 +0800

    [master] change: Add time usage string utils
    Change-Id: I6778b09355b4611a3ca61fe66108fa4ec0a455e4

--- src/main/java/me/ycdev/androidlib/utils/DateTimeUtils.java
+++ src/main/java/me/ycdev/androidlib/utils/DateTimeUtils.java
@@ -12,4 +12,37 @@ public class DateTimeUtils {
     public static String generateFileName(long sysTime) {
         return new SimpleDateFormat("yyyyMMdd-HHmmss-SSS", Locale.US).format(new Date(sysTime));
     }
+
+    /**
+     * Format the time usage to string like "1d17h37m3s728ms"
+     * @param timeUsageMs
+     * @return
+     */
+    public static String getReadeableTimeUsage(long timeUsageMs) {
+        long millisecondsLeft = timeUsageMs % 1000;
+        if (timeUsageMs == millisecondsLeft) {
+            return millisecondsLeft + "ms";
+        }
+
+        long seconds = timeUsageMs / 1000;
+        long secondsLeft = seconds % 60;
+        if (secondsLeft == seconds) {
+            return secondsLeft + "s" + millisecondsLeft + "ms";
+        }
+
+        long minutes = seconds / 60;
+        long minutesLeft = minutes % 60;
+        if (minutesLeft == minutes) {
+            return minutesLeft + "m" + secondsLeft + "s" + millisecondsLeft + "ms";
+        }
+
+        long hours = minutes / 60;
+        long hoursLeft = hours % 24;
+        if (hoursLeft == hours) {
+            return hoursLeft + "h" + minutesLeft + "m" + secondsLeft + "s" + millisecondsLeft + "ms";
+        }
+
+        long days = hours / 24;
+        return days + "d" + hoursLeft + "h" + minutesLeft + "m" + secondsLeft + "s" + millisecondsLeft + "ms";
+    }
 }
