efa56cfe04c6d7a8ef3db0c36b422d75d898716a
Yongce Tu <yongce.tu@gmail.com>
2015-02-26 20:26:24 +0800

    change: Modify ListAdapterBase

--- lib/src/main/java/me/ycdev/android/lib/common/ui/base/ListAdapterBase.java
+++ lib/src/main/java/me/ycdev/android/lib/common/ui/base/ListAdapterBase.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,11 +14,17 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public abstract class ListAdapterBase<T> extends BaseAdapter {
+    protected Context mContext;
     protected LayoutInflater mInflater;
     protected List<T> mList;
 
-    public ListAdapterBase(@NonNull LayoutInflater inflater) {
-        mInflater = inflater;
+    public ListAdapterBase(@NonNull Context cxt) {
+        mContext = cxt;
+        if (cxt instanceof Activity) {
+            mInflater = ((Activity) cxt).getLayoutInflater();
+        } else {
+            mInflater = LayoutInflater.from(cxt);
+        }
     }
 
     public void setData(@Nullable List<T> data) {
